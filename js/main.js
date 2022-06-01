40660ab6e1f8e9b6c48f8b382b941e9b901b79b0
GeorgeYakunenko <92179123+GeorgeYakunenko@users.noreply.github.com>
2022-06-01 19:06:25 +0200

    add js and html

---
+++ js/main.js
@@ -0,0 +1,549 @@
+
+$(document).ready(function () {
+  //Для липкого меню
+  $(window).scroll(function () {
+    if ($(this).scrollTop() >= 90) {
+      $(".header__inner").addClass("fix");
+    } else {
+      $(".header__inner").removeClass("fix");
+    }
+  });
+  //конец липкого меню
+});
+
+
+$ (function(){
+  
+  $(".user__nav-link-menu").on("click", function () {
+    $(".menu__list").toggleClass("menu__list--active");
+  });
+
+  $('.product-tabs__top-item').on('click', function(e){
+    e.preventDefault();
+    $('.product-tabs__top-item').removeClass('product-tabs__top-item--active');
+    $(this).addClass('product-tabs__top-item--active');
+    
+    $('.product-tabs__item').removeClass('product-tabs__item--active');
+    $($(this).attr('href')).addClass('product-tabs__item--active');
+  });
+
+  $(".recent-item__stars").rateYo({
+    rating: 4,
+    starWidth: "11px",
+    normalFill: "#d6d6d6",
+    ratedFill: "#ffcc00",
+    readOnly: true,
+    spacing: "6px",
+  });
+
+  $(".product-one__star").rateYo({
+    rating: 4,
+    starWidth: "17px",
+    normalFill: "#d6d6d6",
+    ratedFill: "#ffcc00",
+    readOnly: true,
+    spacing: "6px",
+  });
+  
+  // кнопки 
+   $(document).on('click', '.input-number__minus', function () {
+        let total = $(this).next();
+        if (total.val() > 1) {
+            total.val(+total.val() - 1);
+        }
+    });
+    
+    $(document).on('click', '.input-number__plus', function () {
+        let total = $(this).prev();
+        total.val(+total.val() + 1);
+    });
+    
+    document.querySelectorAll('.input-number__input').forEach(function (el) {
+        el.addEventListener('input', function () {
+            this.value = this.value.replace(/[^\d]/g, '');
+        });
+    });
+
+
+  $('.product-slider__thumb').slick({
+  slidesToShow: 3,
+  slidesToScroll: 1,
+  arrows: false,
+  focusOnSelect: true,
+  vertical: true,
+  draggable:false,
+  asNavFor: '.product-slider__big'
+  });
+
+  $(".product-slider__big").slick({
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    draggable: false,
+    arrows: false,
+    fade: true,
+    asNavFor: ".product-slider__thumb",
+    responsive: [
+      {
+        breakpoint: 1201,
+        settings: {
+          autoplay: true,
+          draggable: false,
+        },
+      },
+    ],
+  });
+  
+  $(".filter-price__slider").ionRangeSlider({
+        type: "double",
+        min: 0,
+        max: 1000,
+        from: 200,
+        to: 500,
+        grid: true,
+        prefix: "$",
+         onChange: function (data) {
+            $(".filter-price__from").text(data.from);
+            $(".filter-price__to").text(data.to);   
+        },
+    });
+
+  $('.top-slider__inner').slick({
+    dots: true,
+    arrows: false,
+    fade:true,
+    autoplay:true,
+    autoplaySpeed: 3000,
+  });
+
+  $(".related__items").slick({
+    arrows: true,
+    infinite: true,
+    slidesToShow: 4,
+    slidesToScroll: 1,
+    responsive: [
+      {
+        breakpoint: 1050,
+        settings: {
+          slidesToShow: 3,
+          slidesToScroll: 1,
+          infinite: true,
+        },
+      },
+      {
+        breakpoint: 860,
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 1,
+        },
+      },
+      {
+        breakpoint: 620,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1,
+        },
+      },
+    ],
+  });
+
+  $(".shop__filter-btn").on('click', function () {
+    $(".shop__filters").toggleClass("shop__filters--active");
+  });
+
+  
+  
+
+  var containerEl1 = document.querySelector('[data-ref="container-1"]');
+  var containerEl2 = document.querySelector('[data-ref="container-2"]');
+ 
+  var config = {
+    controls: {
+      scope: 'local'
+    }
+  };
+ 
+  var mixer1 = mixitup(containerEl1, config);
+  var mixer2 = mixitup(containerEl2, config);
+
+}
+
+);
+
+let map;
+
+function initMap() {
+  map = new google.maps.Map(document.getElementById("map"), {
+    center: { lat: -34.397, lng: 150.644 },
+    zoom: 8,
+    styles: [
+      {
+        featureType: "all",
+        elementType: "all",
+        stylers: [
+          {
+            visibility: "on",
+          },
+        ],
+      },
+      {
+        featureType: "all",
+        elementType: "labels",
+        stylers: [
+          {
+            visibility: "off",
+          },
+          {
+            saturation: "-100",
+          },
+        ],
+      },
+      {
+        featureType: "all",
+        elementType: "labels.text.fill",
+        stylers: [
+          {
+            saturation: 36,
+          },
+          {
+            color: "#000000",
+          },
+          {
+            lightness: 40,
+          },
+          {
+            visibility: "off",
+          },
+        ],
+      },
+      {
+        featureType: "all",
+        elementType: "labels.text.stroke",
+        stylers: [
+          {
+            visibility: "off",
+          },
+          {
+            color: "#000000",
+          },
+          {
+            lightness: 16,
+          },
+        ],
+      },
+      {
+        featureType: "all",
+        elementType: "labels.icon",
+        stylers: [
+          {
+            visibility: "off",
+          },
+        ],
+      },
+      {
+        featureType: "administrative",
+        elementType: "geometry.fill",
+        stylers: [
+          {
+            color: "#000000",
+          },
+          {
+            lightness: 20,
+          },
+        ],
+      },
+      {
+        featureType: "administrative",
+        elementType: "geometry.stroke",
+        stylers: [
+          {
+            color: "#000000",
+          },
+          {
+            lightness: 17,
+          },
+          {
+            weight: 1.2,
+          },
+        ],
+      },
+      {
+        featureType: "landscape",
+        elementType: "geometry",
+        stylers: [
+          {
+            color: "#000000",
+          },
+          {
+            lightness: 20,
+          },
+        ],
+      },
+      {
+        featureType: "landscape",
+        elementType: "geometry.fill",
+        stylers: [
+          {
+            color: "#4d6059",
+          },
+        ],
+      },
+      {
+        featureType: "landscape",
+        elementType: "geometry.stroke",
+        stylers: [
+          {
+            color: "#4d6059",
+          },
+        ],
+      },
+      {
+        featureType: "landscape.natural",
+        elementType: "geometry.fill",
+        stylers: [
+          {
+            color: "#4d6059",
+          },
+        ],
+      },
+      {
+        featureType: "poi",
+        elementType: "geometry",
+        stylers: [
+          {
+            lightness: 21,
+          },
+        ],
+      },
+      {
+        featureType: "poi",
+        elementType: "geometry.fill",
+        stylers: [
+          {
+            color: "#4d6059",
+          },
+        ],
+      },
+      {
+        featureType: "poi",
+        elementType: "geometry.stroke",
+        stylers: [
+          {
+            color: "#4d6059",
+          },
+        ],
+      },
+      {
+        featureType: "road",
+        elementType: "geometry",
+        stylers: [
+          {
+            visibility: "on",
+          },
+          {
+            color: "#7f8d89",
+          },
+        ],
+      },
+      {
+        featureType: "road",
+        elementType: "geometry.fill",
+        stylers: [
+          {
+            color: "#7f8d89",
+          },
+        ],
+      },
+      {
+        featureType: "road.highway",
+        elementType: "geometry.fill",
+        stylers: [
+          {
+            color: "#7f8d89",
+          },
+          {
+            lightness: 17,
+          },
+        ],
+      },
+      {
+        featureType: "road.highway",
+        elementType: "geometry.stroke",
+        stylers: [
+          {
+            color: "#7f8d89",
+          },
+          {
+            lightness: 29,
+          },
+          {
+            weight: 0.2,
+          },
+        ],
+      },
+      {
+        featureType: "road.arterial",
+        elementType: "geometry",
+        stylers: [
+          {
+            color: "#000000",
+          },
+          {
+            lightness: 18,
+          },
+        ],
+      },
+      {
+        featureType: "road.arterial",
+        elementType: "geometry.fill",
+        stylers: [
+          {
+            color: "#7f8d89",
+          },
+        ],
+      },
+      {
+        featureType: "road.arterial",
+        elementType: "geometry.stroke",
+        stylers: [
+          {
+            color: "#7f8d89",
+          },
+        ],
+      },
+      {
+        featureType: "road.local",
+        elementType: "geometry",
+        stylers: [
+          {
+            color: "#000000",
+          },
+          {
+            lightness: 16,
+          },
+        ],
+      },
+      {
+        featureType: "road.local",
+        elementType: "geometry.fill",
+        stylers: [
+          {
+            color: "#7f8d89",
+          },
+        ],
+      },
+      {
+        featureType: "road.local",
+        elementType: "geometry.stroke",
+        stylers: [
+          {
+            color: "#7f8d89",
+          },
+        ],
+      },
+      {
+        featureType: "transit",
+        elementType: "geometry",
+        stylers: [
+          {
+            color: "#000000",
+          },
+          {
+            lightness: 19,
+          },
+        ],
+      },
+      {
+        featureType: "water",
+        elementType: "all",
+        stylers: [
+          {
+            color: "#2b3638",
+          },
+          {
+            visibility: "on",
+          },
+        ],
+      },
+      {
+        featureType: "water",
+        elementType: "geometry",
+        stylers: [
+          {
+            color: "#2b3638",
+          },
+          {
+            lightness: 17,
+          },
+        ],
+      },
+      {
+        featureType: "water",
+        elementType: "geometry.fill",
+        stylers: [
+          {
+            color: "#24282b",
+          },
+        ],
+      },
+      {
+        featureType: "water",
+        elementType: "geometry.stroke",
+        stylers: [
+          {
+            color: "#24282b",
+          },
+        ],
+      },
+      {
+        featureType: "water",
+        elementType: "labels",
+        stylers: [
+          {
+            visibility: "off",
+          },
+        ],
+      },
+      {
+        featureType: "water",
+        elementType: "labels.text",
+        stylers: [
+          {
+            visibility: "off",
+          },
+        ],
+      },
+      {
+        featureType: "water",
+        elementType: "labels.text.fill",
+        stylers: [
+          {
+            visibility: "off",
+          },
+        ],
+      },
+      {
+        featureType: "water",
+        elementType: "labels.text.stroke",
+        stylers: [
+          {
+            visibility: "off",
+          },
+        ],
+      },
+      {
+        featureType: "water",
+        elementType: "labels.icon",
+        stylers: [
+          {
+            visibility: "off",
+          },
+        ],
+      },
+    ]
+  });
+}
+
+window.initMap = initMap;
+
+
+
